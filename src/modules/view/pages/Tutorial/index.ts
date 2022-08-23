399f6b101f7cfddca634212233ecb86a8704885e
Igor Shaymukhametov <shaym@yandex.ru>
2022-08-23 17:38:13 +0500

    feat: add tutorial layout

--- src/modules/view/pages/Tutorial/index.ts
+++ src/modules/view/pages/Tutorial/index.ts
@@ -18,8 +18,35 @@ const renderTutorial = async () => {
 
   const template = `
     <section class="tutorial section container">
-      ${content}
-    </section>
+      <div class="tutorial__header">
+        <div class="tutorial__difficulties-bar">
+          <button class="btn btn-darken difficulty-btn">A1</button>
+          <button class="btn btn-darken difficulty-btn">A2</button>
+          <button class="btn btn-darken difficulty-btn">B1</button>
+          <button class="btn btn-darken difficulty-btn">B2</button>
+          <button class="btn btn-darken difficulty-btn">C1</button>
+          <button class="btn btn-darken difficulty-btn">C2</button>
+        </div>
+        <div class="tutorial__games-bar">
+          <button class="btn btn-neutral">Аудиовызов</button>
+          <button class="btn btn-neutral">Спринт</button>
+        </div>
+      </div>
+      <div class="tutorial__body">
+        ${content}
+      </div>
+      <div class="tutorial__footer">
+        <div class="tutorial__pagination">
+          <button class="btn">1</button>
+          <button class="btn">2</button>
+          <button class="btn">3</button>
+          <button class="btn">4</button>
+          <button class="btn">5</button>
+          <button class="btn">...</button>
+          <button class="btn">30</button>
+        </div>
+      </div>
+    </section>    
   `;
   return template;
 };
