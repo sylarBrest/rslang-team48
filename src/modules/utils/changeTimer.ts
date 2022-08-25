8c905accade61e072c7d10e8ef5b03993676c6bd
justgrace <ivan.koliada69@gmail.com>
2022-08-25 11:12:53 +0300

    fix: bag with timer

--- src/modules/utils/changeTimer.ts
+++ src/modules/utils/changeTimer.ts
@@ -5,16 +5,17 @@ import showGameResult from './showGameResult';
 const changeTimer = () => {
   let counter = ANSWER_TIME;
 
-  const value = <HTMLElement>document.querySelector('.game__sprint-timer');
-
   const timerID = setInterval(() => {
-    if (value.textContent === ZERO) {
+    const value = <HTMLElement>document.querySelector('.game__sprint-timer');
+
+    if (!value) {
       clearInterval(timerID);
-
+    } else if (value.textContent === ZERO) {
+      clearInterval(timerID);
       showGameResult();
+    } else {
+      value.innerHTML = String(--counter);
     }
-
-    value.innerHTML = String(--counter);
   }, SECOND);
 };
 
