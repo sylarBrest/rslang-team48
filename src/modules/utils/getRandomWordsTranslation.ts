e0c2eef6d3b95912c41959b2fdf986c109569e90
justgrace <ivan.koliada69@gmail.com>
2022-08-25 18:25:13 +0300

    feat: add random words translation

---
+++ src/modules/utils/getRandomWordsTranslation.ts
@@ -0,0 +1,24 @@
+import { temporalSprintWordsData } from '@modules/store/temporalData/temporalSprintWordsData';
+import getRandomInteger from './getRandomInteger';
+
+const getRandomWordsTranslation = () => {
+  const { translationOptions } = temporalSprintWordsData;
+  const index = getRandomInteger(0, translationOptions.length);
+  const {
+    word, wordTranslateOne, wordTranslateTwo, wordTranslateThree,
+    wordTranslateFour, wordTranslateFive,
+  } = translationOptions[index];
+
+  temporalSprintWordsData.translationOptions = translationOptions.filter((_, idx) => idx !== index);
+
+  return {
+    word,
+    wordTranslateOne,
+    wordTranslateTwo,
+    wordTranslateThree,
+    wordTranslateFour,
+    wordTranslateFive,
+  };
+};
+
+export default getRandomWordsTranslation;
