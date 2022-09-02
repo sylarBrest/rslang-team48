ffd5c0ac5f4613b999428243cc23adc8abd5fd21
justgrace <ivan.koliada69@gmail.com>
2022-09-02 20:01:00 +0300

    fix: change words data local value

--- src/modules/store/wordsData/initWordsData.ts
+++ src/modules/store/wordsData/initWordsData.ts
@@ -1,5 +1,9 @@
+import { ZERO } from '@constants';
 import { getWordsData } from './getWordsData';
 
-const wordsDataLocal = getWordsData();
+const wordsDataLocal = getWordsData() || {
+  group: ZERO,
+  page: ZERO,
+};
 
 export default wordsDataLocal;
