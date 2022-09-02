98797387c16cfbc9483be210e6275905da2553ee
justgrace <ivan.koliada69@gmail.com>
2022-09-02 17:18:02 +0300

    refactor: user data local export

--- src/modules/store/userData/index.ts
+++ src/modules/store/userData/index.ts
@@ -2,6 +2,6 @@ export { default as clearUserData } from './clearUserData';
 
 export { default as getUserData } from './getUserData';
 
-export * from './initUserData';
+export { default as userDataLocal } from './initUserData';
 
 export { default as updateUserData } from './updateUserData';
