6d1b40b816952f9c0c5a7da57e2d65543bc4b4e3
Igor Shaymukhametov <shaym@yandex.ru>
2022-09-05 12:58:26 +0500

    feat: add Ivan info

--- src/modules/view/pages/Team/index.ts
+++ src/modules/view/pages/Team/index.ts
@@ -2,8 +2,8 @@ import './style.scss';
 
 const renderTeam = () => `
   <section class="container section">
-    <h2>О команде</h2>
-    <div class="team__header">        
+    <div class="team__header">
+      <h2>О команде</h2>
     </div>
     <div class="team__body">      
       <div class="team-card">        
@@ -27,8 +27,19 @@ const renderTeam = () => `
           </span>
         </h3>
         <div class="team-card__image team-card__image_ivan"></div>
-        <div class="team-card__badges"></div>
-        <p class="team-card__info"></p>        
+        <div class="team-card__badges">
+          <div class="team-card__badge">Спринт</div>
+          <div class="team-card__badge">Аудиовызов</div>
+          <div class="team-card__badge">Авторизация</div>
+          <div class="team-card__badge">Учебник</div>
+          <div class="team-card__badge">Статистика</div>
+          <div class="team-card__badge">Бэкенд</div>
+        </div>        
+        <p class="team-card__info">
+          Искал возможность путешествовать и работать удаленно - нашел себя в разработке.
+          В проекте занимался разработкой игры спринт, игры аудиовызов, участие в разработке 
+          статистики, авторизации, подготовка для работы с сервером
+        </p>        
       </div>
       <div class="team-card">
         <h3 class="team-card__heading">Igor Shaymukhametov
@@ -36,8 +47,7 @@ const renderTeam = () => `
             <a href="https://github.com/knyazigor"><span class="contact contact_gh"></span></a>
             <a href="https://t.me/knyazigor"><span class="contact contact_tg"></span></a>
           </span>
-        </h3>
-        
+        </h3>        
         <div class="team-card__image team-card__image_igor"></div>
         <div class="team-card__badges">
           <div class="team-card__badge">Главная</div>
